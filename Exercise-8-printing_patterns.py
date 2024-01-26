5c88ba68f6aa3ce7dbdb030dc9c4b0789ae97a50
unknown <miggy.payumo26@gmail.com>
2024-01-26 22:42:50 +0800

    Code finalized and Labeled

--- Exercise-8-printing_patterns.py
+++ Exercise-8-printing_patterns.py
@@ -6,8 +6,11 @@
 # 4 4 4 4
 # 5 5 5 5 5
 
+# Loop for until what number we will print.
 for number_amount in range(6):
+
+# Loop for repeating the numbers.
     for i in range(number_amount):
         print (number_amount, end=" ") 
-
+# For printing the pattern
     print("\n")
